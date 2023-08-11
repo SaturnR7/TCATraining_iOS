aa32d3afa3511c4a5380e9a7fae5da3114b71979
SaturnR7 <hidemasa.kobayashi@gmail.com>
2023-08-11 10:18:35 +0900

    Initial Commit

---
+++ TCATraining/TCATrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATrainingApp.swift
+//  TCATraining
+//
+//  Created by Hidemasa Kobayashi on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct TCATrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
